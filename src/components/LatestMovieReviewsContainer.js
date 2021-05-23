06f36ce07934cc7b867c4abe6191a5e8559d6145
cpach96 <cpach96@gmail.com>
2021-05-22 21:59:41 -0400

    commit tree da39a64d7b35e5d65c973e7689adb9f238346f9f

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,4 +6,17 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component{
+
+    state = {
+        reviews: []
+    }
+
+    render(){
+        return(
+            <div className="latest-movie-reviews"></div>
+        )
+    }
+}
+
+export default LatestMovieReviewsContainer;
